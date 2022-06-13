52df161267dd8e4c6ce56287d1a5ebafc9061689
Daniela Tsoi <tsoi.daniela@gmail.com>
2022-06-12 21:58:48 -0400

    updated solutions

--- jaden-casing-strings.js
+++ jaden-casing-strings.js
@@ -9,12 +9,6 @@
 
 //My solution
 String.prototype.toJadenCase = function () {
-  var splitStr = str.toLowerCase().split(' ');
-  for (var i = 0; i < splitStr.length; i++) {
-      // You do not need to check if i is larger than splitStr length, as your for does that for you
-      // Assign it back to the array
-      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-  }
-  // Directly return the joined string
-  return splitStr.join(' '); 
+  const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+  return str2;
   };
